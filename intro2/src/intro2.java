ad035149700afa22bc17e0185e79a7544bfa3a42
Öner Kırbaçlı <113859732+OnerKirbacli@users.noreply.github.com>
2022-10-08 22:17:02 +0300

    Add files via upload

---
+++ intro2/src/intro2.java
@@ -0,0 +1,12 @@
+public class intro2 {
+
+public static void main(String[] args) {
+	int ogrenciSayisi = 12;
+	String mesaj ="Ogrenci sayisi : ";
+	System.out.println(mesaj + ogrenciSayisi);
+	System.out.println(mesaj + ogrenciSayisi);
+	System.out.println(mesaj + ogrenciSayisi);
+	
+	
+}
+}
---
+++ intro2/src/intro2/intro2.java
@@ -0,0 +1,18 @@
+package intro2;
+
+public class intro2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ogrenciSayisi= 12;
+		String mesaj ="Ogrenci sayisi : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		
+				
+
+	}
+
+}
